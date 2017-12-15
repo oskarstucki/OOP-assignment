102b80c7b2b2aa100df2f2340d5aa2fd7fcab2f9
Sami Kohvakka <34503681+samikoh@users.noreply.github.com>
2017-12-15 14:48:09 +0200

    Added getters and setters to abstract class DeliveryClass.

--- src/parcel_system/DeliveryClass.java
+++ src/parcel_system/DeliveryClass.java
@@ -6,27 +6,72 @@ public abstract class DeliveryClass extends Parcel {
     /**
      * Default constructor
      */
-    public DeliveryClass() {
+    public DeliveryClass(double height, double length, double depth, double weight) {
+        super(height,length,depth,weight);
+
+    }
+
+
+    protected double weightLimit;
+    protected double heightLimit;
+    protected double depthLimit;
+    protected double lengthLimit;
+
+    /**
+     * @param weightLimit
+     */
+    public void setWeightLimit(double weightLimit) {
+        this.weightLimit = weightLimit;
     }
 
     /**
      *
      */
-    protected double WeightLimit;
+    public double getWeightLimit(){
+        return this.weightLimit;
+    }
+
+    /**
+     * @param heightLimit
+     */
+    public void setHeightLimit(double heightLimit) {
+        this.heightLimit = heightLimit;
+    }
 
     /**
      *
      */
-    protected double HeightLimit;
+    public double getHeightLimit() {
+        return this.heightLimit;
+    }
+
+    /**
+     * @param depthLimit
+     */
+    public void setDepthLimit(double depthLimit) {
+        this.depthLimit = depthLimit;
+    }
 
     /**
      *
      */
-    protected double DepthLimit;
+    public double getDepthLimit() {
+        return this.depthLimit;
+    }
 
     /**
      *
+     * @param lengthLimit
      */
-    protected double LengthLimit;
+    public void setLengthLimit(double lengthLimit) {
+        this.lengthLimit = lengthLimit;
+    }
 
+    /**
+     *
+      * @return
+     */
+    public double getLengthLimit() {
+        return this.lengthLimit;
+    }
 }
