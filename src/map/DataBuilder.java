ff76f583b2c2acea09d84ad76a93a170bb2cc382
Sami Kohvakka <34503681+samikoh@users.noreply.github.com>
2017-12-16 20:51:27 +0200

    Added method to save storage status to a file and load that file when program is started.

--- src/map/DataBuilder.java
+++ src/map/DataBuilder.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 
 public class DataBuilder {
 
-    private ArrayList<SmartPost> smartPosts = new ArrayList<>();
+    private static ArrayList<SmartPost> smartPosts = new ArrayList<>();
 
     /**
      *  Default constructor
@@ -86,7 +86,7 @@ public class DataBuilder {
 
     }
 
-    public ArrayList<String> returnCities(){
+    public static ArrayList<String> returnCities(){
         ArrayList<String> listOfCities = new ArrayList<String>();
        for(SmartPost post: smartPosts){
            listOfCities.add(post.getCity());
