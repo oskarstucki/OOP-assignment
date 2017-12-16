b30eddbbf0199ff8eff1ece4633e796cd53b0cf7
oskar <osakar94@hotmail.com>
2017-12-16 14:53:29 +0200

    Internetsite is showing

--- src/controller/tab/Tab1Controller.java
+++ src/controller/tab/Tab1Controller.java
@@ -6,17 +6,27 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import controller.MainController;
+import javafx.scene.web.WebView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class Tab1Controller {
 	
 	private MainController main;
-	
+
+    @FXML private WebView mapView;
 	@FXML public Label lbl1;
 	@FXML private TextField txt1;
 	@FXML private Button btn1Save;
 	@FXML private Button btn1Send;
-	
-	@FXML private void btn1SaveClicked(ActionEvent event) {
+
+
+    @FXML public void initialize() {
+        mapView.getEngine().load("http://www.lut.fi");
+    }
+
+    @FXML private void btn1SaveClicked(ActionEvent event) {
 		System.out.println("Btn 1 save clicked");
 		
 		lbl1.setText(txt1.getText());
