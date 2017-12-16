3a5dfcee49d52e76ba381c36d5b81938e5ad9106
Sami Kohvakka <34503681+samikoh@users.noreply.github.com>
2017-12-16 12:37:04 +0200

    Modified ThirdClass.

--- src/parcel_system/ThirdClass.java
+++ src/parcel_system/ThirdClass.java
@@ -1,6 +1,7 @@
 package parcel_system;
 
 import java.util.Random;
+import java.time.LocalDate;
 
 public class ThirdClass extends DeliveryClass {
 
@@ -26,12 +27,22 @@ public class ThirdClass extends DeliveryClass {
 
 
     /**
-     * Some non-fragile packets will break too. Probability of breaking is 5%.
+     * Some non-fragile packets will break too. Probability of breaking during winter is 10%
+     * and otherwise it is 5%.
      */
     private void didNonFragileBreak(){
+        // Draw a random number between [0,100[
         Random rnd = new Random();
         int p = rnd.nextInt(100);
-        if (p < 5) {
+
+        // Get month
+        LocalDate date = LocalDate.now();
+        int month = date.getMonthValue();
+
+        // During winter months parcels are more likely to break.
+        if ((month < 4 && p < 10) || (month > 8 && p < 10)) {
+            this.broken = 1;
+        } else if (p < 5) {
             this.broken = 1;
         } else {
             this.broken = 0;
