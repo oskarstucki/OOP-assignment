743f7ee9c23e91e610e8b2becd8c4434d47b3795
Sami Kohvakka <34503681+SamiKoh@users.noreply.github.com>
2017-12-16 20:09:41 +0200

    Added method to get default Items.

--- src/stuff/DefaultItems.java
+++ src/stuff/DefaultItems.java
@@ -4,21 +4,24 @@ import java.util.ArrayList;
 
 public class DefaultItems {
 
+    private ArrayList<Item> items = new ArrayList<>();
+
     public DefaultItems(){
-        createItems();
+         createItems();
     }
 
-    private ArrayList<Item> createItems(){
+    private void createItems(){
         Item dvd = new Item(1.5,19, 13.5,0.1, 0,"Autot 2 DVD");
         Item frisbee = new Item(3, 20, 20, 0.3, 0, "Frisbee");
         Item mug = new Item(7, 12, 7, 0.2, 1, "Iittala Taika-muki");
         Item fridge = new Item(60, 180, 67, 63, 0, "Electrolux jääkaappi");
-        ArrayList<Item> items = new ArrayList<>();
         items.add(dvd);
         items.add(frisbee);
         items.add(mug);
         items.add(fridge);
-        return items;
     }
 
+    public ArrayList<Item> getItems() {
+        return items;
+    }
 }
