b6c2142bfb797639f80865e193deaab35310481e
Sami Kohvakka <34503681+samikoh@users.noreply.github.com>
2017-12-17 19:00:10 +0200

    Coordinate class added.

--- src/map/GeopointAdder.java
+++ src/map/GeopointAdder.java
@@ -1,11 +1,36 @@
 package map;
 
+
+
 public class GeopointAdder {
+
+    private double[] coordinates = new double[2];
+
 
     /**
      * Default constructor
      */
-    public GeopointAdder() {
+    public GeopointAdder(double lat, double lng) {
+        coordinates[0] = lat;
+        coordinates[1] = lng;
+    }
 
+    public void setLatitude(float latitude){
+        coordinates[0] = latitude;
+    }
+
+    public void setLongitude(float longitude){
+        coordinates[1] = longitude;
+    }
+
+    public double getLatitude(){
+        return coordinates[0];
+    }
+    public double getLongitude(){
+        return coordinates[1];
+    }
+
+    public double[] getCoordinates() {
+        return coordinates;
     }
 }
